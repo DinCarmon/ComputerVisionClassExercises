e9374d4dbf8f237a340c036a81495578342316d7
benso <bensolomon10@gmail.com>
2025-01-31 10:33:55 +0200

    bonus

--- Project/solution/bonus_model.py
+++ Project/solution/bonus_model.py
@@ -1,6 +1,7 @@
 """Define your architecture here."""
 import torch
-from models import SimpleNet
+from torch import nn
+from torchvision import models
 
 
 def my_bonus_model():
@@ -8,8 +9,24 @@ def my_bonus_model():
 
     Do not change the model load line.
     """
-    # initialize your model:
-    model = SimpleNet()
+    chosen_model_name = "mobilenetv2"
+
+    if chosen_model_name == "mobilenetv2":
+        # MobileNetV2 Pretrained
+        model = models.mobilenet_v2(pretrained=True)
+        model.classifier[1] = nn.Linear(model.last_channel, 2)  # Modify output layer
+
+    elif chosen_model_name == "resnet18":
+        # ResNet-18 Pretrained
+        model = models.resnet18(pretrained=True)
+        model.fc = nn.Linear(model.fc.in_features, 2)  # Modify output layer
+
+    elif chosen_model_name == "squeezenet":
+        # SqueezeNet Pretrained
+        model = models.squeezenet1_0(pretrained=True)
+        model.classifier[1] = nn.Conv2d(512, 2, kernel_size=(1, 1))  # Modify output layer
+        model.num_classes = 2
+
     # load your model using exactly this line (don't change it):
     model.load_state_dict(torch.load('checkpoints/bonus_model.pt')['model'])
     return model
